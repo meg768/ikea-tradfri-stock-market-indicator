4473beaa458c9c9529f8fbd310515e5adcda450b
meg768 <meg768@users.noreply.github.com>
2019-01-29 20:19:41 +0100

    -

--- index.js
+++ index.js
@@ -1,62 +1,6 @@
 #!/usr/bin/env node
 
-var Gateway    = require('./gateway.js');
-var fs         = require('fs');
-var Path       = require('path');
-var StockMarketIndicator = require('./stock-market-indicator.js');
-
-class App {
-
-    constructor() {
-        require('dotenv').config();
-
-        this.configFile = Path.join(Path.dirname(__filename), 'config.json');
-        this.gateway = new Gateway(this.log);
-        this.config = JSON.parse(fs.readFileSync(this.configFile));
-        this.indicators = [];
-
-    }
-
-    log() {
-        console.log.apply(this, arguments);
-    }
-
-    run() {
-		return new Promise((resolve, reject) => {
-            this.gateway.connect().then(() => {
-                return Promise.resolve();
-            })
-            .then(() => {
-
-                this.config.indicators.forEach((config) => {
-                    this.indicators.push(new StockMarketIndicator(this.log, this.gateway, config));
-                });
-
-                var promise = Promise.resolve();
-
-                this.indicators.forEach((indicator) => {
-
-                    promise = promise.then(() => {
-                        return indicator.start();
-                    });
-                });
-
-                return promise;
-
-            })
-            .then(() => {
-                resolve();
-            })
-            .catch((error) => {
-                this.gateway.disconnect();                
-                reject(error);
-            });
-        });
-    }
-
-
-}
-
+var App = require('./app.js');
 
 var app = new App();
 
