fe853eb17ef93a5a100cd363361f96e2a5453a41
meg768 <meg768@users.noreply.github.com>
2019-09-04 22:08:52 +0200

    -

--- stock-market-indicator.js
+++ stock-market-indicator.js
@@ -145,7 +145,7 @@ module.exports = class StockMarketIndicator extends Indicator {
                     var now = new Date();
                     var hour = now.getHours();
 
-                    if (hour >= 9 && hour <= 21)
+                    if (hour >= 9 && hour <= 22)
                         return color;
                     else
                         return ({hue:0, saturation:0, luminance:0});
