479bbf8d9b070669a6e8ec4f99954d59cd61afcf
meg768 <meg768@users.noreply.github.com>
2019-09-04 18:13:28 +0200

    -

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ module.exports = class App {
     }
 
     log() {
-        if (this.config && this.config.debug)
+//        if (this.config && this.config.debug)
             console.log.apply(this, arguments);
     }
 
