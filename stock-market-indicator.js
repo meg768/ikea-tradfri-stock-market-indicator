1f12260b325ed3648f924a5de9c6651185150de4
meg768 <meg768@users.noreply.github.com>
2019-09-04 22:53:50 +0200

    -

--- stock-market-indicator.js
+++ stock-market-indicator.js
@@ -53,7 +53,7 @@ module.exports = class StockMarketIndicator extends Indicator {
         var change     = Math.max(-1, Math.min(1, quote.change));
         var hue        = change >= 0 ? 120 : 0;
         var saturation = 100;
-        var luminance  = 5 + (Math.abs(change) * 45);
+        var luminance  = 0 + (Math.abs(change) * 50);
 
 
         return {hue:hue, saturation:saturation, luminance:luminance}
